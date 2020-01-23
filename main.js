9ef543e98f63a9b2a17efc5e2e77b0aa66a02f65
Nemanja Danev <nemanja.danev.93@gmail.com>
2020-01-23 18:53:58 +0100

    used ajax to fetch data from db.json file to show projects

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-// class Object {
-//     constructor() {
-//         const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+window.addEventListener("load", loadProjects);
 
-//         for (let i = 0; i < numbers.length; i++) {
-//             console.log(i);
-//         }
-//     }
-// }
+function loadProjects() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "./db.json", true);
+
+    xhr.onload = function() {
+        if (this.status == 200) {
+            var projects = JSON.parse(this.responseText);
+
+            console.log(projects);
+
+            var output = "";
+
+            for (var i in projects) {
+                output +=
+                    '<div class="col-md-4">' +
+                    '<div class="project-card">' +
+                    '<a target="_blank" href=" ' +
+                    projects[i].website +
+                    ' ">' +
+                    '<img src="' +
+                    projects[i].img +
+                    '">' +
+                    "</a>" +
+                    "<a target='_blank' class='card-btn' href=" +
+                    projects[i].website +
+                    ">" +
+                    projects[i].title +
+                    "</a>" +
+                    "</div>" +
+                    "</div>";
+            }
+        }
+
+        document.getElementById("project-cards").innerHTML = output;
+    };
+
+    xhr.send();
+}
